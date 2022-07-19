7424dea1c228838b8db03aefd304ac50417bd162
Vinita Parasrampuria <91571551+vinitaparasrampuria@users.noreply.github.com>
2022-07-18 21:57:19 -0400

    Add files via upload

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/HomeDesk1.png";
+import homeLogo from "../../Assets/student-girl.jpg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
